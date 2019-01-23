5883b6ce0dde1e06fdb9e043697b9744741a1631
Dion Bosschieter <dbosschieter@transip.nl>
2019-01-23 14:42:38 +0100

    Add controller,input and event structs

--- main.go
+++ main.go
@@ -14,6 +14,24 @@ type JoyStickInputLine struct {
     index   int
 }
 
+type C64JoyStick struct {
+    path   string
+    reader *bufio.Reader
+    file   *os.File
+}
+
+type ButtonEvent struct {
+    buttonNumber int
+    isDown       bool
+}
+
+type StickEvent struct {
+    isY      bool
+    isCenter bool
+    buttonA  bool
+    buttonB  bool
+}
+
 func (j *JoyStickInputLine) SetByte(character byte) {
     if j.index < BUFLENGTH {
         j.buffer[j.index] = character
@@ -21,33 +39,79 @@ func (j *JoyStickInputLine) SetByte(character byte) {
     }
 }
 
-var reader *bufio.Reader
+func (j *JoyStickInputLine) ByteSet(i int) bool {
+    return j.buffer[i] == 1
+}
 
-func read() {
+func (j *JoyStickInputLine) ButtonNumber() int {
+    return int(j.buffer[7])
+}
+
+func (j *JoyStickInputLine) ButtonGroup() byte {
+    return j.buffer[6]
+}
+
+func (j *JoyStickInputLine) StickMatches(group1 byte, group2 byte) bool {
+    return j.buffer[4] == group1 && j.buffer[5] == group2
+}
+
+func (j *C64JoyStick) Read() {
     stickInputLine := JoyStickInputLine{}
 
     for i:=0;i<BUFLENGTH;i++ {
-        character,err := reader.ReadByte()
+        character,err := j.reader.ReadByte()
         if err != nil {
             log.Fatal(err)
         }
         stickInputLine.SetByte(character)
     }
 
-    fmt.Println("Got a new line")
-    fmt.Println(stickInputLine.buffer)
+    
+    if stickInputLine.ButtonGroup() == 1 {
+        fmt.Println("Button pressed")
+        event := ButtonEvent{
+            buttonNumber: stickInputLine.ButtonNumber(),
+            isDown: stickInputLine.ByteSet(4),
+        }
+        fmt.Println(event)
+    } else if stickInputLine.ButtonGroup() == 2 {
+        fmt.Println("Joystick moved")
+        event := StickEvent{
+            isY:      stickInputLine.ByteSet(7),
+            isCenter: stickInputLine.StickMatches(0, 0),
+            buttonA:  stickInputLine.StickMatches(1, 128),
+            buttonB:  stickInputLine.StickMatches(255, 127),
+        }
+        fmt.Println(event)
+    }
+}
+
+func (j *C64JoyStick) Close() {
+    err := j.file.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
 }
 
 // Reads input from joystick 0
 func main() {
-    file, err := os.Open("/dev/input/js0")
+    controller := OpenController("/dev/input/js0")
+
+    defer controller.Close()
+    for {
+        controller.Read()
+    }
+}
+
+func OpenController(path string) C64JoyStick {
+    controller := C64JoyStick{path:path}
+    file, err := os.Open(controller.path)
     if err != nil {
         log.Fatal(err)
     }
-    defer file.Close()
 
-    reader = bufio.NewReader(file)
-    for {
-        read()
-    }
+    controller.file = file
+    controller.reader = bufio.NewReader(file)
+
+    return controller
 }
